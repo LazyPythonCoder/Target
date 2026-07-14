1f833fc5c062e139d67dc81dcdd38e05850764c5
Роман Тихонов <99688019+LazyPythonCoder@users.noreply.github.com>
2026-07-14 16:42:47 +0300

    Add files via upload

---
+++ servo_180_2relay.ino
@@ -0,0 +1,88 @@
+#include <Servo.h>
+#include <ezButton.h>
+
+#define RELAY_PIN 6 // PIN to Relay (Program) 
+#define RELAY_PINRC 7 // PIN to Relay (RC) 
+#define SERVO_PIN 8 // PIN to SERVO
+#define TARGET_PIN 5 // PIN to Relay for target
+
+Servo myservo; 
+
+int pos = 0;
+
+ezButton actSwitch(A2);
+
+void setup() { 
+    pinMode(RELAY_PIN, INPUT);
+    pinMode(RELAY_PINRC, INPUT);
+    Serial.begin(9600); 
+    actSwitch.setDebounceTime(40);
+    myservo.attach(SERVO_PIN); 
+    myservo.write(180); 
+    Serial.write("Положение сервопривода при активации=");
+    Serial.println(myservo.read());
+}  
+void loop() {  
+   actSwitch.loop();
+   int angle = myservo.read();
+   
+   if (actSwitch.isPressed()) {
+      Serial.println("Act Switch pressed");
+      Serial.print("Состояние реле=");
+      Serial.println(digitalRead(RELAY_PIN));
+      if (angle<=30) {
+        myservo.write(180);
+        delay(100);
+      } else {
+        myservo.write(0);
+        delay(100);
+      }
+      
+      Serial.print("Положение сервопривода из за Act Switch=");
+      Serial.println(myservo.read());
+   }
+   int relayState = digitalRead(RELAY_PIN); 
+  //  Serial.println(relayState);
+   if (relayState == HIGH) {
+    Serial.print("Состояние реле=");
+    Serial.println(relayState);
+    angle =  myservo.read();
+   //  Serial.print("угол=");
+   //  Serial.println(angle);
+     if (angle<30) {
+         myservo.write(180);
+      }
+      delay(2000);
+      Serial.print("Положение сервопривода из за реле=");
+      Serial.println(myservo.read());
+   }
+
+   int relayStateRC = digitalRead(RELAY_PINRC); 
+   if (relayStateRC == HIGH) {
+      Serial.print("Состояние релеRC=");
+      Serial.println(relayStateRC);
+      angle =  myservo.read();
+   //  Serial.print("угол=");
+   //  Serial.println(angle);
+     if (angle<30) {
+         myservo.write(180);
+      } else {
+          myservo.write(0);
+      }
+      delay(2000);
+      Serial.print("Положение сервопривода из за релеRC=");
+      Serial.println(myservo.read());
+   }
+
+   int targetState = digitalRead(TARGET_PIN); 
+   if (targetState == HIGH) {
+      Serial.println("Target!!!");
+      angle =  myservo.read();
+      if (angle>70) {
+         myservo.write(0);
+         delay(100);
+      }
+      Serial.println("Положение сервопривода из за реле цели=");
+      Serial.println(myservo.read());
+   }
+}  
